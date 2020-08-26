df97204fc02ca2845ccb266a958ed69794f7d4b1
omermazor144 <52323243+omermazor144@users.noreply.github.com>
2020-08-26 19:11:06 +0300

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoDB: 'mongodb+srv://OnlineDatingApp1:omermazor1211@onlinedatingapp.gcz5a.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    FacebookAppID : '302290234333663',
+    FacebookAppSecret : 'c56d21225342bdd6c156a62a4371dc2d'
+
+};
